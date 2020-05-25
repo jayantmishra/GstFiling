911271b63a6ff7de2b978811823aaf303e7b615f
jayantMishra <jayant.neymar@gmail.com>
2020-05-25 15:05:09 +0530

    initializing

---
+++ src/main/java/com/me/urbanmart/GSTFiling/GstFilingApplication.java
@@ -0,0 +1,13 @@
+package com.me.urbanmart.GSTFiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstFilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstFilingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/urbanmart/GSTFiling/GstFilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.urbanmart.GSTFiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstFilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
